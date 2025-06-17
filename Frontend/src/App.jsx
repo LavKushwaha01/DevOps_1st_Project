7a5ad9947ebe39f910bd77986394e4c3d2765115
Lav Kushwaha <lavkumar062@gmail.com>
2025-06-17 11:34:17 +0530

    fe

--- Frontend/src/App.jsx
+++ Frontend/src/App.jsx
@@ -17,7 +17,7 @@ function App() {
     }
 
     try {
-      const response = await fetch('http://localhost:3000/user', {
+      const response = await fetch('http://34.131.95.237:3000/user', {
         method: 'POST',
         headers: {
           'Content-Type': 'application/json'
